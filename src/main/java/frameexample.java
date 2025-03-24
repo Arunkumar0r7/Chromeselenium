2719ebcc7e1ddca6641116bd91ee801f63753df5
Arunkumar0r7 <arunemy71@gmail.com>
2025-03-24 17:49:39 +0530

    learned about frame and i frame and implemented iframe with id and with xpath

---
+++ src/main/java/frameexample.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class frameexample {
+    public static void main(String[] args){
+
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://letcode.in/frame");
+        //doing with xpath
+        WebElement myframe = driver.findElement(By.xpath("//iframe[@src='frameui']"));
+        driver.switchTo().frame(myframe);
+
+        /// with iframe id
+//        driver.switchTo().frame("firstFr");
+        driver.findElement(By.name("fname")).sendKeys("Arun");
+        driver.findElement(By.name("lname")).sendKeys("Kumar");
+
+        driver.switchTo().frame(0);
+        driver.findElement(By.name("email")).sendKeys("Test@123");
+
+
+
+
+    }
+}
--- src/main/java/org/example/opengoogle.java
+++ src/main/java/org/example/opengoogle.java
@@ -1,12 +1,7 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
 
-public class Opengoogle{
+public class opengoogle{
     public static void main(String[] args){
 
         WebDriver driver = new ChromeDriver();
